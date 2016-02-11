d5ea840077515ee2405c1dd52a3c8813044dca5c
Luis De Avila <luis@ideanerd.com>
2016-02-11 15:27:07 -0800

    Added some comments to the code... because best practices.

--- app/turtle/turtle.js
+++ app/turtle/turtle.js
@@ -1,3 +1,10 @@
+/**
+ * Constructs a turtle object that can be used to store the locaiton of the turtle on the canvas
+ *
+ * @param spec A specification object {x: number, y: number}. x,y default to 300,300 if no specificaiton is given
+ *
+ * @returns {Object}
+ */
 let create = function (spec = {}) {
   // canvas will be 600,600 pixels in size. We default to starting in middle
   let {x = 300, y = 300} = spec;
@@ -7,10 +14,20 @@ let create = function (spec = {}) {
     y
   };
 
+  /**
+   * Retrieves the turtle state
+   *
+   * @returns {{x, y}}
+   */
   let get = function () {
     return turtle;
   };
 
+  /**
+   *  Updates the turtle state
+   *
+   * @param spec {x: number, y: number} where x,y are optional and have no effect if omitted
+   */
   let set = function (spec = {}) {
     let {x = turtle.x, y = turtle.y} = spec;
 
